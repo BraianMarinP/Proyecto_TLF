8e0c7e6192de21473908f6cb94a5a869b391663f
BraianMarinP <braianmarinp7@gmail.com>
2024-05-27 22:14:53 -0500

    Segunda version procesamiento de cadenas, y generacion de resultado por tokens o categorias

--- ProyectoTLF.py
+++ ProyectoTLF.py
@@ -28,49 +28,77 @@ operadores_relacionales = ["::", "!!", "♦", "♣", "♦::", "♣::"]
 operadores_asignacion = ["=", "⊕=", "⊥=", "⊗=", "⊘=", "○="]
 simbolos_abrir = ["(", "[", "{"]
 simbolos_cerrar = [")", "]", "}"]
-simbolo_terminal = [";"]
-separador_sentencia = ["\\n"]
-simbolos = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", 
-            "]", "^", "_", "`", "{", "|", "}", "~", "¿", "¡", "“", "”", "‘", "’"]
-
-
-directorio = {
-    "palabras_reservadas_bucles": ["Mientras ", "Para "],
-    "palabras_decision": ["si ", "sino ", "otro "]
-}
 
 resultado = ""
 
-
-class analizador_lexico:
+class AnalizadorLexico:
     def recortar_cadena(self, cadena1, cadena2):
-        if cadena2.startswith(" "):
+        while cadena2.startswith(" "):
             cadena2 = cadena2[1:]
         if cadena2.startswith(cadena1):
-            return cadena2[len(cadena1):]
-        return cadena2
+            return cadena2[len(cadena1):], True
+        return cadena2, False
+
+    def es_identificador(self, cadena):
+        if not cadena:
+            return False
+        if not (cadena[0].isalpha()):  # debe comenzar con una letra
+            return False
+        for char in cadena[1:]:
+            if not (char.isalnum()):  # debe ser alfanumérico
+                return False
+        return True
 
     def analizar(self):
-        # Ejemplo de uso
-        catgoria1 = ["estaEsMi"]
-        catgoria2 = ["cadena", "cualquiera"]
-        categorias = [catgoria1, catgoria2]
-        cadena2 = " estaEsMicadenacualquiera"
+        global resultado
+        categorias = [
+            palabras_reservadas_bucles, palabras_decision, palabras_clase,
+            metodos_publicos, metodos_privados, palabra_entero, palabras_reales,
+            palabra_cadena, palabra_caracter, comentario, operadores_aritmeticos,
+            operadores_logicos, operadores_relacionales, operadores_asignacion,
+            simbolos_abrir, simbolos_cerrar
+        ]
+        
+        nombres_categorias = [
+            "palabras_reservadas_bucles", "palabras_decision", "palabras_clase",
+            "metodos_publicos", "metodos_privados", "palabra_entero", "palabras_reales",
+            "palabra_cadena", "palabra_caracter", "comentario", "operadores_aritmeticos",
+            "operadores_logicos", "operadores_relacionales", "operadores_asignacion",
+            "simbolos_abrir", "simbolos_cerrar"
+        ]
+        
+        cadena2 = " identificador123 Mientras { dob} cadena"
 
-        for categoria in categorias:
-            for cadena1 in categoria:
-                cadena2 = self.recortar_cadena(cadena1, cadena2)
-                print(f"Después de recortar '{cadena1}': '{cadena2}'")
+        def procesar_cadena(cadena2):
+            global resultado
+            for nombre_categoria, categoria in zip(nombres_categorias, categorias):
+                for cadena1 in categoria:
+                    cadena2, recortado = self.recortar_cadena(cadena1, cadena2)
+                    if recortado:
+                        resultado += f"{nombre_categoria}: '{cadena1}'\n"
+                        return cadena2, True
+            return cadena2, False
 
+        while cadena2:
+            cadena2, procesado = procesar_cadena(cadena2)
+            if not procesado and cadena2 and not cadena2[0].isspace():
+                # Extraer identificador o verificamos si esa parte de la cadena
+                # es un identificador
+                i = 0
+                while i < len(cadena2) and cadena2[i].isalnum():
+                    i += 1
+                identificador = cadena2[:i]
+                if self.es_identificador(identificador):
+                    resultado += f"Identificador encontrado: '{identificador}'\n"
+                    cadena2 = cadena2[i:]
+                else:
+                    cadena2 = cadena2[1:]
+            elif not procesado:
+                cadena2 = cadena2[1:]
+
+        print(resultado)
         print(f"Cadena final: '{cadena2}'")
 
-
-                        
-
-
 if __name__ == "__main__":
-    analizador = analizador_lexico()
+    analizador = AnalizadorLexico()
     analizador.analizar()
-
-
-    
